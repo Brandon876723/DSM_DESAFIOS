842ddb5ed8802694a61dbd7725ff34192725beca
Brandon <brandoncenteno534@gmail.com>
2026-08-15 17:36:51 -0600

    modify activity_salario.xml

--- Desafio1/app/src/main/java/com/example/desafio1_dsm/SalarioActivity.kt
+++ Desafio1/app/src/main/java/com/example/desafio1_dsm/SalarioActivity.kt
@@ -1,4 +1,10 @@
 package com.example.desafio1_dsm
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Button
+import android.widget.TextView
 class SalarioActivity {
+
 }
