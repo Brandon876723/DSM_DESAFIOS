878bbdf761893f53cea4b3863d5f31cba2a0f145
Brandon <brandoncenteno534@gmail.com>
2026-09-12 12:22:56 -0600

    añadir icono personalizado

--- Desafio2/app/build.gradle.kts
+++ Desafio2/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "com.example.agencia_viajes"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
